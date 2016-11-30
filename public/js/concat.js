56bc5e9a3c6c8f06adb0385c7c0032d527c307c1
Oscar Lopez <oscarlopez@despegar.com>
2016-11-30 17:25:07 -0300

    correccion prod host

--- public/js/concat.js
+++ public/js/concat.js
@@ -51,6 +51,27 @@ hotelServiceModules.factory('HotelService',['$http','EnvironmentConfig',function
         }
     }
 }]);
+var resultsModule = angular.module('resultsModule');
+
+resultsModule.controller('HotelController', ['$scope', 'HotelService', function ($scope, HotelService) {
+
+    this.getHotelRate = function () {
+        
+        HotelService.getHotelRate()
+            .then(function (result) {
+                console.log(result);
+                $scope.hotels = result.data.hotels || [];
+            })
+            .catch(function (err) {
+                console.log(err);
+            })
+    };
+
+    this.init = function () {
+        this.getHotelRate();
+    }
+
+}]);
 var hotelFilters = angular.module('hotelsResultsApp');
 
 hotelFilters.directive('nav',[function() {
@@ -83,27 +104,6 @@ hotelFilters.directive('nav',[function() {
 
         }]
     }
-}]);
-var resultsModule = angular.module('resultsModule');
-
-resultsModule.controller('HotelController', ['$scope', 'HotelService', function ($scope, HotelService) {
-
-    this.getHotelRate = function () {
-        
-        HotelService.getHotelRate()
-            .then(function (result) {
-                console.log(result);
-                $scope.hotels = result.data.hotels || [];
-            })
-            .catch(function (err) {
-                console.log(err);
-            })
-    };
-
-    this.init = function () {
-        this.getHotelRate();
-    }
-
 }]);
 var hotelFilters = angular.module('HotelFilterModule');
 
